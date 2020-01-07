a3d0694636c8eaef293002da3154478cdd077dee
윤기동 <yun@yungidong-ui-MacBookPro.local>
2020-01-08 01:30:50 +0900

    kidongyun

---
+++ workspace/devlog/interpark/request_page/_posts/2020-1-7-request-page-with-redirection.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Interpark - Request page with redirection."
+date:   2020-01-07 11:21:54 +0900
+categories: interpark http post request
+---
+
+> I usually felt the needs of this request page whenever I develop the some project. Actually When we make the any api services, Usually we use the _postman_ or the _swagger_ tools. but These are only for the api test so I couldn't take real UI view when I used them. so I made this page really simplely but It's powerful. <br><br> If you should test your site entering from another site, then use this.
+
+[Request Page]()
---
+++ workspace/devlog/interpark/request_page/res/requestPage.js
@@ -0,0 +1,66 @@
+jQry = jQuery.noConflict();
+
+function request() {
+    const form = document.createElement("form");
+
+    let charset = getCharset();
+    let parameterType = getParameterType();
+    let url = getUrl();
+    let parameters = getQueryString().split("&");
+
+    form.setAttribute("charset", charset);
+    form.setAttribute("method", parameterType);
+    form.setAttribute("action", url);
+
+    for(let i=0; i < parameters.length; i++) {
+        let name = parameters[i].split("=")[0];
+        let value = parameters[i].split("=")[1];
+
+        let param = document.createElement("input");
+
+        param.setAttribute("type", "hidden");
+        param.setAttribute("name", name);
+        param.setAttribute("value", value);
+
+        form.appendChild(param);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+function getUrl() {
+    return jQry("#url").val();
+}
+
+function getQueryString() {
+    return jQry("#parameter").val();
+}
+
+function setQueryString(queryString) {
+    jQry("#parameter").val(queryString);
+}
+
+function getParameterType() {
+    if(jQry("#getBtn").parents("label").hasClass("active") === true)
+        return "GET";
+
+    if(jQry("#postBtn").parents("label").hasClass("active") === true)
+        return "POST";
+}
+
+function getCharset() {
+    return jQry("#charset").html();
+}
+
+function setCharset(charset) {
+    jQry("#charset").html(charset);
+}
+
+function encode() {
+    setQueryString(encodeURIComponent(getQueryString()));
+}
+
+function decode() {
+    setQueryString(decodeURIComponent(getQueryString()));
+}
